462a49ae26278c388a9d2db6ffe9c285ed3aed1d
Steve Yardumian <steve@360mediadirect.com>
2024-04-26 15:42:55 +0000

    move from yarn to npm; update all packages

--- src/__tests__/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Locks, locks } from '../index'
-
-describe('index', () => {
-  // TODO: More tests
-  it('exports an instance with an acquire function', () => {
-    expect(locks).toBeInstanceOf(Locks)
-    expect(locks).toHaveProperty('acquire')
-  })
-})
